a0204cea64af02ca97420b6a8c37032f9e5c0d73
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-06-02 10:10:53 +0530

    Create functions to get data to app

--- src/Container/ProductListing.js
+++ src/Container/ProductListing.js
@@ -1,10 +1,24 @@
 import React from 'react' ;
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import axios from 'axios';
 import ProductComponent from './ProductComponent';
 
 const ProductList = () => {
+    const getStoreItemData = async() => {
+        const response = await axios
+            .get('https://fakestoreapi.com/products')
+            .catch(err => {console.log("Error : "+ err)}) ;
+        
+            console.log(response) ;
+    }
+
     const products = useSelector((state) => state) ;
     console.log(products) ;
+
+    useEffect(() =>{
+
+    }, []) ;
 
     return(
         <div className="ui grid container">
